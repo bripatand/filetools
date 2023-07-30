3e7e618a43ebdeb35a4883bc3e806133c6073106
Patrick Briand <bripatand@gmail.com>
2023-07-30 12:13:22 +0200

    Add audio converter to mp3

---
+++ Convert2Mp3.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path, PurePath
+import glob
+import argparse
+
+
+def main(args):
+
+	#sourcefolder = '/mnt/p/Sports/Fighting/Kravmaga/KravMagaGlobal/KravMagaGlobalFR/KravMagaUniversity'
+	sourcefolder = args.input_dir
+	
+	if args.filter:
+		filter = args.filter
+	else:
+		filter = '*.flac'
+
+	globarg = sourcefolder + '/**/' + filter
+
+	if args.out_folder:
+		destinationfolder = args.out_folder
+	else:
+		destinationfolder = '.'
+
+	files = glob.glob(globarg, recursive = True)
+
+	for file in files:
+			
+		# Pure path objects provide path-handling operations which don’t actually access a filesystem
+		filepath = PurePath(file)
+		ext = filepath.suffix
+		parentpath = filepath.parent
+		stem = filepath.stem
+		relativepath = parentpath.relative_to(sourcefolder)
+
+		if args.suffix:
+			suffix = args.suffix
+		else:
+			suffix = ''
+
+		# Must use Path and not PurePath to be able to call mkdir method on the object later
+		destfolder = Path(destinationfolder).joinpath(relativepath)
+		deststem = destfolder.joinpath(stem, suffix)
+		destpath = str(deststem) + '.mp3'
+		
+		print(f"ext {ext}")
+		print(f"parentpath {parentpath}")
+		print(f"stem {stem}")
+		print(f"relative {relativepath}")
+		print(f"destfolder {destfolder}")
+		print(f"destpath {destpath}")
+
+		#ffmpeg -i "$f" -vf scale=1080:720 -crf 20 -c:a copy "$OUTPUT_FOLDER/$video"
+
+		# Use single quote and double quote inside as some path/filename can contain single quote
+
+		
+		command = f'ffmpeg -i "{file}" -ab 320k -map_metadata 0 -id3v2_version 3 "{destpath}"'
+		print ('Command:' + command)
+
+		if(not args.test):
+			destfolder.mkdir(parents=True, exist_ok=True)
+
+			os.system(command)
+			
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_dir", help="Location of the input folder.")
+	parser.add_argument("-o", "--out-folder", help="Output folder. Default is same as input.")
+	parser.add_argument("-f", "--filter", help="pattern to filter files (default '*.flac').")
+	parser.add_argument("-s", "--suffix", help="Suffix added to output filename.")
+	parser.add_argument("-t", "--test", action='store_true', help="just testing.")
+	args = parser.parse_args()
+
+	main(args)
