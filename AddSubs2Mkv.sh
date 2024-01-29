f2c1176bfa860b080cfb46657901a4172a34e067
Patrick Briand <bripatand@gmail.com>
2024-01-29 19:26:25 +0100

    Add script to insert subs in video file

---
+++ AddSubs2Mkv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file=$(basename $1)
+
+base=${file%.*}
+
+extension="${file##*.}"
+
+#echo $file $base $extension
+
+output="${base}_WithSubs.${extension}"
+
+ffmpeg -i $1 -f srt -i $2 -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt $output
+
